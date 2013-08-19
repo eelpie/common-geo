ab076714096732f1841212e3431a98ed15a09841
Tony McCrae <tony@eelpieconsulting.co.uk>
2013-08-20 08:43:40 +1200

    Updating asserts to match current data.

--- src/test/java/uk/co/eelpieconsulting/common/geo/NominatimGeocodingIT.java
+++ src/test/java/uk/co/eelpieconsulting/common/geo/NominatimGeocodingIT.java
@@ -21,16 +21,18 @@ public class NominatimGeocodingIT {
 	@Test
 	public void canResolveBuildingNameToLocation() {
 		List<Place> results = service.resolvePlaceName("St James Presbyterian Church, Newtown, Wellington");
+		
 		final Place firstMatch = results.get(0);
-		assertEquals("St James' Presbyterian Church, Adelaide Road, Newtown, Wellington, Wellington Region, 6021, New Zealand", firstMatch.getAddress());
+		assertEquals("St James' Presbyterian Church, Adelaide Road, Newtown, Wellington, 6021, New Zealand/Aotearoa", firstMatch.getAddress());
 		assertEquals(301919657, firstMatch.getOsmId().getId());
-		assertEquals("place_of_worship", firstMatch.getOsmId().getType());
+		assertEquals("node", firstMatch.getOsmId().getType());
 	}
 
 	@Test
 	public void canResolveAddressWithMultipleResults() throws Exception {
 		List<Place> results = service.resolvePlaceName("Civic Square, Wellington");
-		assertEquals(2, results.size());
+		
+		assertEquals(7, results.size());
 	}
 	
 }
